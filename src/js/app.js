4ef7b85ba74c4e1232ad8a3b3809d3ddf9e23a5e
Walai <eaawato@students.eaaa.dk>
2020-03-30 17:25:52 +0200

    Add window event listener in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,19 @@
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("../sw_fetchall.js")
+//     .then(function(registration) {
+//       console.log("Service Worker registered with scope:", registration.scope);
+//     })
+//     .catch(function(err) {
+//       console.log("Service Worker registration failed:", err);
+//     });
+// }
+
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("../sw_fetchall.js")
-    .then(function(registration) {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch(function(err) {
-      console.log("Service Worker registration failed:", err);
-    });
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../sw_fetchall.js")
+      .then(reg => console.log("Service Worker: Registered"))
+      .catch(err => console.log(`Service Worker: Error: ${err}`));
+  });
 }
