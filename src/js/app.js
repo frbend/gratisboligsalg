af74a37e095d91dd2178e0aac16f5106535fe5a1
Walai <eaawato@students.eaaa.dk>
2020-03-30 18:16:17 +0200

    Test sw registration wo window event listener

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,10 @@
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("../sw_fetchall.js")
-//     .then(function(registration) {
-//       console.log("Service Worker registered with scope:", registration.scope);
-//     })
-//     .catch(function(err) {
-//       console.log("Service Worker registration failed:", err);
-//     });
-// }
-
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("../sw_fetchall.js")
-      .then(reg => console.log("Service Worker: Registered"))
-      .catch(err => console.log(`Service Worker: Error: ${err}`));
-  });
+  navigator.serviceWorker
+    .register("../sw_fetchall.js")
+    .then(function(registration) {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch(function(err) {
+      console.log("Service Worker registration failed:", err);
+    });
 }
